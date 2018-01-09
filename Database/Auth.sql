0d5ab729734f23fd3739d49cb193822ac39c16ae
Xijezu <xijezu@hotmail.de>
2018-01-09 09:07:47 +0100

    Cleaned Auth & Telecaster for easy import

--- Database/Auth.sql
+++ Database/Auth.sql
@@ -13,9 +13,6 @@
 
 -- Exportiere Daten aus Tabelle Auth.Accounts: ~2 rows (ungefähr)
 /*!40000 ALTER TABLE `Accounts` DISABLE KEYS */;
-INSERT INTO `Accounts` (`account_id`, `login_name`, `password`, `block`, `withdraw_remain_time`, `age`, `auth_ok`, `pcbang`, `last_login_server_idx`, `event_code`, `server_list_mask`, `result`, `ip`, `game_code`) VALUES
-	(1, 'xijezu', 'c9c8b57bdc080a7847f1cffa3fd6e9a0', 0, 0, 0, 1, 0, 0, 0, '0', 0, 0, '0'),
-	(1337, 'xijtest', 'c9c8b57bdc080a7847f1cffa3fd6e9a0', 0, 0, 23, 1, 0, 0, 0, '0', 0, 0, '0');
 /*!40000 ALTER TABLE `Accounts` ENABLE KEYS */;
 
 -- Exportiere Daten aus Tabelle Auth.IPLog: ~0 rows (ungefähr)
