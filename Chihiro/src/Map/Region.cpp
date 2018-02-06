d057816f2a0f494fecc44945252294f7ff33eb65
Xijezu <marcel@xijezu.com>
2018-02-06 20:50:18 +0100

    Added comment about regions

--- Chihiro/src/Map/Region.cpp
+++ Chihiro/src/Map/Region.cpp
@@ -91,6 +91,13 @@ uint Region::DoEachMovableObject(WorldObjectFunctor &fn)
 void Region::addObject(WorldObject *obj, std::vector<WorldObject *> *v)
 {
     MX_UNIQUE_GUARD writeGuard(i_lock);
+    // The region has to be correct
+    // No matter what. So we're ASSERTing
+    // to just crash if it isn't.
+    // If this ASSERT fails, fix your fricking code
+    // Having invalid region pointers
+    // is an absolute no go
+    ASSERT(obj->pRegion == nullptr);
     v->emplace_back(obj);
     obj->AddToWorld();
     obj->pRegion = this;
@@ -100,8 +107,15 @@ void Region::addObject(WorldObject *obj, std::vector<WorldObject *> *v)
 void Region::removeObject(WorldObject *obj, std::vector<WorldObject *> *v)
 {
     MX_UNIQUE_GUARD writeGuard(i_lock);
+    // The region has to be correct
+    // No matter what. So we're ASSERTing
+    // to just crash if it isn't.
+    // If this ASSERT fails, fix your fricking code
+    // Having invalid region pointers
+    // is an absolute no go
     ASSERT(!v->empty());
     ASSERT((*v)[obj->region_index]->GetHandle() == obj->GetHandle());
+    ASSERT(obj->pRegion == this);
 
     if(v->back()->GetHandle() != obj->GetHandle())
     {
