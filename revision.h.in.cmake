c64974e6b2ca27bc4ce03570ed9edb3b928f06e9
Xijezu <marcel@xijezu.com>
2018-02-18 21:31:03 +0100

    Fixed copyright in revision cmake

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -3,8 +3,8 @@
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
  #define _BRANCH                    "@rev_branch@"
- #define VER_COMPANYNAME_STR        "Xijezu"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2016-2018 Xijezu"
+ #define VER_COMPANYNAME_STR        "NGemity"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2016-2018 NGemity"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
