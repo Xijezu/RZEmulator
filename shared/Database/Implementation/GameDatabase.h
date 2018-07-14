255878f432a2103f3f0822846c34866677c004e1
Xijezu <marcel@xijezu.com>
2018-07-14 20:06:52 +0200

    Some missing files...

---
+++ shared/Database/Implementation/GameDatabase.h
@@ -0,0 +1,30 @@
+#ifndef __GAMEDATABASE_H__
+#define __GAMEDATABASE_H__
+
+#include "MySQLConnection.h"
+
+enum GameDatabaseStatements : uint32
+{
+    /*  Naming standard for defines:
+    {DB}_{SET/DEL/ADD/REP}_{Summary of data changed}
+    When updating more than one field, consider looking at the calling function
+    name for a suiting suffix.
+    */
+            MAX_GAMEDATABASE_STATEMENTS
+};
+
+class GameDatabaseConnection : public MySQLConnection
+{
+    public:
+        typedef GameDatabaseStatements Statements;
+
+//- Constructors for sync and async connections
+        GameDatabaseConnection(MySQLConnectionInfo &connInfo);
+        GameDatabaseConnection(ProducerConsumerQueue<SQLOperation *> *q, MySQLConnectionInfo &connInfo);
+        ~GameDatabaseConnection();
+
+//- Loads database type specific prepared statements
+        void DoPrepareStatements() override;
+};
+
+#endif // _GAMEDATABASE_H_
