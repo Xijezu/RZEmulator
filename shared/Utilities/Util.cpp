393bbda70071519c7dfb5c0df00184c7c1e826d8
Xijezu <marcel@xijezu.com>
2018-07-15 12:47:10 +0200

    Accidentally removed too much

--- shared/Utilities/Util.cpp
+++ shared/Utilities/Util.cpp
@@ -234,6 +234,7 @@ uint32 TimeStringToSecs(const std::string& timestring)
     return secs;
 }
 
+
 void string_replace(std::string& str, const std::string& from, const std::string& to)
 {
     if(from.empty())
@@ -417,6 +418,80 @@ std::wstring GetMainPartOfName(std::wstring wname, uint32 declension)
     }
 
     return wname;
+}
+
+bool utf8ToConsole(const std::string& utf8str, std::string& conStr)
+{
+#if PLATFORM == PLATFORM_WINDOWS
+    std::wstring wstr;
+    if (!Utf8toWStr(utf8str, wstr))
+        return false;
+
+    conStr.resize(wstr.size());
+    CharToOemBuffW(&wstr[0], &conStr[0], wstr.size());
+#else
+    // not implemented yet
+    conStr = utf8str;
+#endif
+
+    return true;
+}
+
+bool consoleToUtf8(const std::string& conStr, std::string& utf8str)
+{
+#if PLATFORM == PLATFORM_WINDOWS
+    std::wstring wstr;
+    wstr.resize(conStr.size());
+    OemToCharBuffW(&conStr[0], &wstr[0], conStr.size());
+
+    return WStrToUtf8(wstr, utf8str);
+#else
+    // not implemented yet
+    utf8str = conStr;
+    return true;
+#endif
+}
+
+bool Utf8FitTo(const std::string& str, std::wstring search)
+{
+    std::wstring temp;
+
+    if (!Utf8toWStr(str, temp))
+        return false;
+
+    // converting to lower case
+    wstrToLower( temp );
+
+    if (temp.find(search) == std::wstring::npos)
+        return false;
+
+    return true;
+}
+
+void utf8printf(FILE* out, const char *str, ...)
+{
+    va_list ap;
+    va_start(ap, str);
+    vutf8printf(out, str, &ap);
+    va_end(ap);
+}
+
+void vutf8printf(FILE* out, const char *str, va_list* ap)
+{
+#if PLATFORM == PLATFORM_WINDOWS
+    char temp_buf[32*1024];
+    wchar_t wtemp_buf[32*1024];
+
+    size_t temp_len = vsnprintf(temp_buf, 32*1024, str, *ap);
+
+    size_t wtemp_len = 32*1024-1;
+    Utf8toWStr(temp_buf, temp_len, wtemp_buf, wtemp_len);
+
+    CharToOemBuffW(&wtemp_buf[0], &temp_buf[0], wtemp_len+1);
+    fprintf(out, "%s", temp_buf);
+#else
+    vfprintf(out, str, *ap);
+#endif
 }
 
 std::string ByteArrayToHexStr(uint8 const* bytes, uint32 arrayLen, bool reverse /* = false */)
