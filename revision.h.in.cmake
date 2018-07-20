8280dca6c104344838a8007c2d3a60916f606712
Xijezu <marcel@xijezu.com>
2018-07-20 23:54:23 +0200

    Some changes for less warnings on Micro$oft²

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -1,10 +1,28 @@
 #pragma once
- #define _HASH                      "@rev_hash@"
- #define _DATE                      "@rev_date@"
- #define _BRANCH                    "@rev_branch@"
- #define VER_COMPANYNAME_STR        "NGemity"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2016-2018 NGemity"
- #define VER_FILEVERSION            0,0,0
- #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
- #define VER_PRODUCTVERSION         VER_FILEVERSION
- #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#if !defined(_HASH)
+# define _HASH                      "@rev_hash@"
+#endif
+#if !defined(_DATE)
+# define _DATE                      "@rev_date@"
+#endif
+#if !defined(_BRANCH)
+# define _BRANCH                    "@rev_branch@"
+#endif
+#if !defined(_VER_COMPANYNAME_STR)
+# define VER_COMPANYNAME_STR        "NGemity"
+#endif
+#if !defined(VER_LEGALCOPYRIGHT_STR)
+# define VER_LEGALCOPYRIGHT_STR     "(c)2016-2018 NGemity"
+#endif
+#if !defined(VER_FILEVERSION)
+# define VER_FILEVERSION            0,0,0
+#endif
+#if !defined(VER_FILEVERSION_STR)
+# define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
+#endif
+#if !defined(VER_PRODUCTVERSION)
+# define VER_PRODUCTVERSION         VER_FILEVERSION
+#endif
+#if !defined(VER_PRODUCTVERSION_STR)
+# define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif
