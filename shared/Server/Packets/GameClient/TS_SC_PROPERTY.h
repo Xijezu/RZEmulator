527b42fb8b6d0c8195aa6c8bb7d00b7f6f6a94fa
Xijezu <marcel@xijezu.com>
2018-11-10 19:03:54 +0100

    Rewriting network code for @glandu2 structs

--- shared/Server/Packets/GameClient/TS_SC_PROPERTY.h
+++ shared/Server/Packets/GameClient/TS_SC_PROPERTY.h
@@ -7,12 +7,12 @@
 // String value include null terminator
 #define TS_SC_PROPERTY_DEF(_) \
 	_(simple) (uint32_t, handle) \
-	_(simple) (bool, is_number) \
+    _(simple) (uint8_t, is_number) \
 	_(string) (name, 16) \
 	_(def)(simple)(int64_t, value) \
 	_(impl)(simple)(int64_t, value, version >= EPIC_4_1_1) \
 	_(impl)(simple)(int32_t, value, version < EPIC_4_1_1) \
-	_(endstring)(string_value, true)
+    _(endstring)(string_value, true, !is_number)
 
 CREATE_PACKET(TS_SC_PROPERTY, 507);
 
