1b62408798afc4d8204ea72b7939f66fdf530a69
Xijezu <marcel@xijezu.com>
2019-01-24 22:19:43 +0100

    Fix for _FULLVERSION

--- cmake/SetDefinitions.cmake
+++ cmake/SetDefinitions.cmake
@@ -17,7 +17,7 @@
 #
 
 # Set build-directive (used in core to tell which buildtype we used)
-add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
+add_definitions(-D_BUILD_DIRECTIVE="${CMAKE_BUILD_TYPE}")
 
 #Set config install path correctly from given path
 string(FIND "${CONF_DIR}" ":" CONF_DIR_ABSOLUTE)
