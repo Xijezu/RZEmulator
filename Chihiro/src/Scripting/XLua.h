9a17bbfe1922308dada5b0738009406338dd3188
YoSiem <patryk.olejnik655@gmail.com>
2023-11-07 18:22:05 +0100

    Fixed my last commit

--- Chihiro/src/Scripting/XLua.h
+++ Chihiro/src/Scripting/XLua.h
@@ -97,6 +97,8 @@ private:
     int32_t SCRIPT_GetItemCode(uint32_t);
     int32_t SCRIPT_UpdateGoldChaos();
     int32_t SCRIPT_LearnAllSkill();
+    void SCRIPT_LearnCreatureAllSkill(sol::variadic_args);
+    void SCRIPT_DropItem(sol::variadic_args args);
     void SCRIPT_SavePlayer();
     uint32_t SCRIPT_InsertItem(sol::variadic_args);
 
@@ -114,8 +116,6 @@ private:
     void SCRIPT_ShowMarket(std::string);
 
     void SCRIPT_InsertGold(sol::variadic_args);
-    void SCRIPT_DropItem(sol::variadic_args);
-
 
     int32_t SCRIPT_GetProperChannelNum(int32_t) { return 0; }
 
